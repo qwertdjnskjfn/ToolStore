1d60787049c8308e7247fd1e8beeeff5bff8eb0f
MajorFox_OvO <112736920+Re0XIAOPA@users.noreply.github.com>
2024-12-24 12:01:13 +0800

    更新

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -24,6 +24,40 @@ const downloadLinks = {
         windows: 'https://github.com/2dust/v2rayN/releases/download/6.33/v2rayN-With-Core.zip',
         android: 'https://github.com/2dust/v2rayNG/releases/download/1.8.9/v2rayNG_1.8.9.apk',
         github: 'https://github.com/2dust/v2rayN'
+    },
+    'singbox': {
+        windows: 'https://github.com/SagerNet/sing-box/releases/download/v1.10.5/sing-box-1.10.5-windows-386.zip',
+        android: 'https://github.com/SagerNet/sing-box/releases/download/v1.10.5/SFA-1.10.5-universal.apk',
+        linux: 'https://github.com/SagerNet/sing-box/releases/download/v1.10.5/sing-box-1.10.5-linux-386.tar.gz',
+        github: 'https://github.com/SagerNet/sing-box'
+    },
+    'FlClash': {
+        windows: 'https://github.com/FlClash/FlClash/releases/download/v1.0.0/FlClash-1.0.0-windows-x64.zip',
+        github: 'https://github.com/FlClash/FlClash'
+    },
+    'Hiddify': {
+        windows: 'https://github.com/hiddify/hiddify-manager/releases/download/v1.0.0/hiddify-manager-1.0.0-windows-x64.zip',
+        github: 'https://github.com/hiddify/hiddify-manager'
+    },
+    'NekoBox': {
+        windows: 'https://github.com/NekoBox-Dev/NekoBox-for-Windows/releases/download/v1.0.0/NekoBox-v1.0.0-x64.exe',
+        github: 'https://github.com/NekoBox-Dev/NekoBox-for-Windows'
+    },
+    'v2rayu': {
+        windows: 'https://github.com/yanue/V2rayU/releases/download/2.1.0/V2rayU.dmg',
+        github: 'https://github.com/yanue/V2rayU'
+    },
+    'shadowrocket': {
+        ios: 'https://apps.apple.com/us/app/shadowrocket/id932747118'
+    },
+    'quantumultx': {
+        ios: 'https://apps.apple.com/us/app/quantumult-x/id1443988620'
+    },
+    'surge5': {
+        ios: 'https://apps.apple.com/us/app/surge-5/id1442620678'
+    },
+    'oneclick': {
+        ios: 'https://apps.apple.com/us/app/oneclick-safe-easy-fast/id1545555197'
     }
 };
 
