ebb368319cccced94f00c55ca17d1f66f9e3aa27
MajorFoxs_OvO <mckekejun@gmail.com>
2025-05-04 21:34:46 +0800

    优化

--- assets/scripts/configs/airport-config.js
+++ assets/scripts/configs/airport-config.js
@@ -41,29 +41,6 @@ const airportDetails = {
                 traffic: '6000G/一次性'
             }
         ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
-            }
-        ],
         link: 'https://xn--mes358a9urctx.com/#/register?code=fY6Y8I3k',
         tags: ['稳定', '高速', '性价比']
     },
@@ -88,29 +65,6 @@ const airportDetails = {
                 price: '10.00',
                 period: '月',
                 traffic: '120G/月'
-            }
-        ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
             }
         ],
         link: 'https://dash.fscloud.homes/#/register?code=LQF59pkU',
@@ -157,29 +111,7 @@ const airportDetails = {
                 traffic: '1000G/月'
             },
         ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
-            }
-        ],
+        
         link: 'https://xn--9kqy92aw5h.com/#/register?code=O0Mcbu1C',
         tags: ['稳定', '高速', '性价比']
     },
@@ -194,29 +126,7 @@ const airportDetails = {
                 traffic: '160G/月'
             }
         ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
-            }
-        ],
+        
         link: 'https://go.dginv.click/#/register?code=FdXt0gA2',
         tags: ['稳定', '高速', '性价比']
     },
@@ -229,29 +139,6 @@ const airportDetails = {
                 price: '15.00',
                 period: '年',
                 traffic: '300G/月'
-            }
-        ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
             }
         ],
         link: 'https://needaycloud.xyz/#/register?code=dWkLWMTf',
@@ -280,29 +167,6 @@ const airportDetails = {
                 traffic: '1536/月'
             }
         ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
-            }
-        ],
         link: 'https://magicat.click/register?code=jVzrdmom',
         tags: ['稳定', '高速', '性价比']
     },
@@ -315,29 +179,6 @@ const airportDetails = {
                 price: '15.00',
                 period: '年',
                 traffic: '300G/月'
-            }
-        ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
             }
         ],
         link: 'https://xingclouds.xyz/#/register?code=L3DcR9kF',
@@ -354,29 +195,6 @@ const airportDetails = {
                 traffic: '120G/月'
             }
         ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
-            }
-        ],
         link: 'https://yunanyun.app/#/register?code=QqGbRwUf',
         tags: ['稳定', '高速', '性价比']
     },
@@ -389,29 +207,6 @@ const airportDetails = {
                 price: '3.58',
                 period: '月',
                 traffic: '100G/月'
-            }
-        ],
-        features: [
-            {
-                title: '全球节点优质覆盖',
-                description: '覆盖全球多个区域，智能优选最佳线路',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg>`
-            },
-            {
-                title: '稳定流媒体解锁',
-                description: '解锁Netflix、Disney+等主流流媒体服务',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM9 8h2v8H9zm4 0h2v8h-2z"/>
-                </svg>`
-            },
-            {
-                title: '无限制客户端',
-                description: '支持多设备同时在线，不限制客户端数量',
-                icon: `<svg viewBox="0 0 24 24">
-                    <path fill="#7c8aff" d="M16 1H8C6.34 1 5 2.34 5 4v16c0 1.66 1.34 3 3 3h8c1.66 0 3-1.34 3-3V4c0-1.66-1.34-3-3-3zm-2 20h-4v-1h4v1zm3.25-3H6.75V4h10.5v14z"/>
-                </svg>`
             }
         ],
         link: 'https://mitce.com/aff.php?aff=3878',
