d0f45b935e2a8f76f982a4ea2f9e7aeae222b307
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2025-06-08 03:26:16 +0000

    Update site content 0ff4ac824a0e09104a2379d20f5e19e1b37d24fa

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 6/7/2025, 3:16:22 AM
+// 最后更新时间: 6/8/2025, 3:26:14 AM
 
 const downloadLinks = {
     "v2box": {
@@ -48,10 +48,10 @@ const downloadLinks = {
         "github": "https://github.com/clash-verge-rev/clash-verge-rev"
     },
     "mihomo party": {
-        "version": "v1.7.4",
-        "windows": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.4/mihomo-party-windows-1.7.4-x64-setup.exe.sha256",
-        "mac": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.4/mihomo-party-macos-1.7.4-x64.pkg.sha256",
-        "linux": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.4/mihomo-party-linux-1.7.4-amd64.deb.sha256",
+        "version": "v1.7.5",
+        "windows": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.5/mihomo-party-windows-1.7.5-x64-setup.exe.sha256",
+        "mac": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.5/mihomo-party-macos-1.7.5-x64.pkg.sha256",
+        "linux": "https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.7.5/mihomo-party-linux-1.7.5-amd64.deb.sha256",
         "github": "https://github.com/mihomo-party-org/mihomo-party"
     },
     "clashmeta": {
@@ -82,11 +82,11 @@ const downloadLinks = {
         "github": "https://github.com/GUI-for-Cores/GUI.for.SingBox"
     },
     "flclash": {
-        "version": "v0.8.84",
-        "windows": "https://github.com/chen08209/FlClash/releases/download/v0.8.84/FlClash-0.8.84-windows-amd64.zip.sha256",
-        "mac": "https://github.com/chen08209/FlClash/releases/download/v0.8.84/FlClash-0.8.84-macos-amd64.dmg.sha256",
-        "linux": "https://github.com/chen08209/FlClash/releases/download/v0.8.84/FlClash-0.8.84-linux-amd64.rpm.sha256",
-        "android": "https://github.com/chen08209/FlClash/releases/download/v0.8.84/FlClash-0.8.84-android-arm64-v8a.apk",
+        "version": "v0.8.85",
+        "windows": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-windows-amd64.zip.sha256",
+        "mac": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-macos-amd64.dmg.sha256",
+        "linux": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-linux-amd64.rpm.sha256",
+        "android": "https://github.com/chen08209/FlClash/releases/download/v0.8.85/FlClash-0.8.85-android-arm64-v8a.apk",
         "github": "https://github.com/chen08209/FlClash"
     },
     "nekobox": {
