f8a81b940c7944589d4f2ce424bb07b7714eedc2
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2025-10-15 03:08:36 +0000

    Update site content fb9efe1898c53a7b8b9462aad16085b077fbf9a9

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 10/14/2025, 3:04:48 AM
+// 最后更新时间: 10/15/2025, 3:08:35 AM
 
 const downloadLinks = {
     "v2box": {
@@ -24,11 +24,11 @@ const downloadLinks = {
         "ios": "https://apps.apple.com/us/app/oneclick-safe-easy-fast/id1545555197"
     },
     "singbox": {
-        "version": "v1.12.9",
-        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.12.9/sing-box-1.12.9-windows-amd64.zip",
-        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.12.9/SFM-1.12.9-universal.dmg",
-        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.12.9/sing-box_1.12.9_linux_amd64.deb",
-        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.12.9/SFA-1.12.9-universal.apk",
+        "version": "v1.12.10",
+        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.12.10/sing-box-1.12.10-windows-amd64.zip",
+        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.12.10/SFM-1.12.10-universal.dmg",
+        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.12.10/sing-box_1.12.10_linux_amd64.deb",
+        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.12.10/SFA-1.12.10-universal.apk",
         "github": "https://github.com/SagerNet/sing-box"
     },
     "hiddify": {
@@ -55,8 +55,8 @@ const downloadLinks = {
         "github": "https://github.com/mihomo-party-org/mihomo-party"
     },
     "clashmeta": {
-        "version": "v2.11.17",
-        "android": "https://github.com/MetaCubeX/ClashMetaForAndroid/releases/download/v2.11.17/cmfa-2.11.17-meta-universal-release.apk",
+        "version": "v2.11.18",
+        "android": "https://github.com/MetaCubeX/ClashMetaForAndroid/releases/download/v2.11.18/cmfa-2.11.18-meta-universal-release.apk",
         "github": "https://github.com/MetaCubeX/ClashMetaForAndroid"
     },
     "surfboard": {
