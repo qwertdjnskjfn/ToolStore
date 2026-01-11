5126f582bcb31ad05cf3d9677e2f2bbbdf5544ed
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2026-01-11 03:54:15 +0000

    Update site content 93ad87d86007ba4cf28cebd50f795cb3ae6fdcdf

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 1/10/2026, 3:36:57 AM
+// 最后更新时间: 1/11/2026, 3:54:14 AM
 
 const downloadLinks = {
     "v2box": {
@@ -76,8 +76,8 @@ const downloadLinks = {
         "github": "https://github.com/2dust/v2rayNG"
     },
     "gui.for.singbox": {
-        "version": "v1.17.0",
-        "windows": "https://github.com/GUI-for-Cores/GUI.for.SingBox/releases/download/v1.17.0/GUI.for.SingBox-windows-amd64.zip",
+        "version": "v1.18.0",
+        "windows": "https://github.com/GUI-for-Cores/GUI.for.SingBox/releases/download/v1.18.0/GUI.for.SingBox-windows-amd64.zip",
         "github": "https://github.com/GUI-for-Cores/GUI.for.SingBox"
     },
     "flclash": {
