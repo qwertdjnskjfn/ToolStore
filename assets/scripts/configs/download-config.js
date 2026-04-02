7d39c0f9f0aa85403d4b53610fe313aa82ecfd1f
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2026-04-02 04:34:21 +0000

    Update site content 83824022f0f6880e43b88fe8d7682c1b3220667c

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 4/1/2026, 4:51:11 AM
+// 最后更新时间: 4/2/2026, 4:34:19 AM
 
 const downloadLinks = {
     "v2box": {
@@ -54,8 +54,8 @@ const downloadLinks = {
         "github": "https://github.com/mihomo-party-org/mihomo-party"
     },
     "clashmeta": {
-        "version": "v2.11.24",
-        "android": "https://github.com/MetaCubeX/ClashMetaForAndroid/releases/download/v2.11.24/cmfa-2.11.24-meta-universal-release.apk",
+        "version": "v2.11.25",
+        "android": "https://github.com/MetaCubeX/ClashMetaForAndroid/releases/download/v2.11.25/cmfa-2.11.25-meta-universal-release.apk",
         "github": "https://github.com/MetaCubeX/ClashMetaForAndroid"
     },
     "surfboard": {
