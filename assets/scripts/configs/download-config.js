5cddd4a8578d668dfac2a93d73566f3836509066
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2026-07-10 06:10:38 +0000

    Update site content 0713e6c13350ab8eef2d192be6347fc6bc9194eb

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 7/9/2026, 6:11:23 AM
+// 最后更新时间: 7/10/2026, 6:10:36 AM
 
 const downloadLinks = {
     "v2box": {
@@ -64,10 +64,10 @@ const downloadLinks = {
         "github": "https://github.com/getsurfboard/surfboard"
     },
     "v2rayn": {
-        "version": "v7.22.7",
-        "windows": "https://github.com/2dust/v2rayN/releases/download/7.22.7/v2rayN-linux-64.zip",
-        "mac": "https://github.com/2dust/v2rayN/releases/download/7.22.7/v2rayN-macos-64.dmg",
-        "linux": "https://github.com/2dust/v2rayN/releases/download/7.22.7/v2rayN-linux-64.deb",
+        "version": "v7.23.3",
+        "windows": "https://github.com/2dust/v2rayN/releases/download/7.23.3/v2rayN-linux-64.zip",
+        "mac": "https://github.com/2dust/v2rayN/releases/download/7.23.3/v2rayN-macos-64.dmg",
+        "linux": "https://github.com/2dust/v2rayN/releases/download/7.23.3/v2rayN-linux-64.deb",
         "github": "https://github.com/2dust/v2rayN"
     },
     "v2rayng": {
