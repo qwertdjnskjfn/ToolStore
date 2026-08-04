6318f74cdca4fe2dbf8b8011230df60aa2ac877b
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2026-08-04 05:20:08 +0000

    Update site content 98fffae3fae8953b837bd5715edafaf51f956d0d

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 8/3/2026, 5:49:08 AM
+// 最后更新时间: 8/4/2026, 5:20:06 AM
 
 const downloadLinks = {
     "v2box": {
@@ -24,11 +24,11 @@ const downloadLinks = {
         "ios": "https://apps.apple.com/us/app/oneclick-safe-easy-fast/id1545555197"
     },
     "singbox": {
-        "version": "v1.13.15",
-        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.13.15/sing-box-1.13.15-windows-amd64.zip",
-        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.13.15/SFM-1.13.15-Universal.pkg",
-        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.13.15/sing-box_1.13.15_linux_amd64.deb",
-        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.13.15/SFA-1.13.15-universal.apk",
+        "version": "v1.13.16",
+        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.13.16/sing-box-1.13.16-windows-amd64.zip",
+        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.13.16/SFM-1.13.16-Universal.pkg",
+        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.13.16/sing-box_1.13.16_linux_amd64.deb",
+        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.13.16/SFA-1.13.16-universal.apk",
         "github": "https://github.com/SagerNet/sing-box"
     },
     "hiddify": {
@@ -59,8 +59,8 @@ const downloadLinks = {
         "github": "https://github.com/MetaCubeX/ClashMetaForAndroid"
     },
     "surfboard": {
-        "version": "v1.1.1",
-        "android": "https://github.com/getsurfboard/surfboard/releases/download/tv-1.1.1/tv-universal-release.apk",
+        "version": "v2.34.1",
+        "android": "https://github.com/getsurfboard/surfboard/releases/download/mobile-2.34.1/mobile-universal-release.apk",
         "github": "https://github.com/getsurfboard/surfboard"
     },
     "v2rayn": {
