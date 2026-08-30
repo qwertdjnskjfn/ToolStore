be1d9aef4b60d37fc11d851ccb9a6c2bb5c4a629
qwertdjnskjfn <qwertdjnskjfn@users.noreply.github.com>
2026-08-30 08:10:52 +0000

    Update site content cd47c7348a40050dea19f746a30ac60e72fa4f37

--- assets/scripts/configs/download-config.js
+++ assets/scripts/configs/download-config.js
@@ -1,5 +1,5 @@
 // 下载链接配置  全是小写
-// 最后更新时间: 8/29/2026, 8:53:44 AM
+// 最后更新时间: 8/30/2026, 8:10:50 AM
 
 const downloadLinks = {
     "v2box": {
@@ -24,11 +24,11 @@ const downloadLinks = {
         "ios": "https://apps.apple.com/us/app/oneclick-safe-easy-fast/id1545555197"
     },
     "singbox": {
-        "version": "v1.13.19",
-        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.13.19/sing-box-1.13.19-windows-amd64.zip",
-        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.13.19/SFM-1.13.19-Universal.pkg",
-        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.13.19/sing-box_1.13.19_linux_amd64.deb",
-        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.13.19/SFA-1.13.19-universal.apk",
+        "version": "v1.13.20",
+        "windows": "https://github.com/SagerNet/sing-box/releases/download/v1.13.20/sing-box-1.13.20-windows-amd64.zip",
+        "mac": "https://github.com/SagerNet/sing-box/releases/download/v1.13.20/SFM-1.13.20-Universal.pkg",
+        "linux": "https://github.com/SagerNet/sing-box/releases/download/v1.13.20/sing-box_1.13.20_linux_amd64.deb",
+        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.13.20/SFA-1.13.20-universal.apk",
         "github": "https://github.com/SagerNet/sing-box"
     },
     "hiddify": {
